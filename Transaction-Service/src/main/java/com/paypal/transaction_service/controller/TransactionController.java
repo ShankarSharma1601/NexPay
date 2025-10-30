1e69561126a89f7039ab60319199e907bf199dc4
ShankarSharma1601 <shankarsharma1601@gmail.com>
2025-10-30 15:55:38 +0530

    User service and Transaction Service Implementation

---
+++ Transaction-Service/src/main/java/com/paypal/transaction_service/controller/TransactionController.java
@@ -0,0 +1,36 @@
+package com.paypal.transaction_service.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paypal.transaction_service.entity.Transaction;
+import com.paypal.transaction_service.service.TransactionService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/transactions/")
+public class TransactionController {
+	private final TransactionService service;
+
+	public TransactionController(TransactionService service) {
+		this.service = service;
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<?> create (@Valid @RequestBody Transaction transaction){
+		Transaction created = service.createTransaction(transaction);
+		return ResponseEntity.ok(created);
+	}
+	
+	@GetMapping("/all")
+	public List<Transaction> getAll(){
+		return service.getAllTransactions();
+	}
+}
